744f8adb87e278e9cbafd6c963c60ed5838abdd6
akshatk3 <ak.akshatk@gmail.com>
2021-06-14 19:47:00 -0400

    First commit

---
+++ jackson fda search engine/src/Results.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Results {
+	private ArrayList<MedClass> results;
+
+	public ArrayList<MedClass> getResults() {
+		return results;
+	}
+
+	public void setResults(ArrayList<MedClass> results) {
+		this.results = results;
+	}
+}
